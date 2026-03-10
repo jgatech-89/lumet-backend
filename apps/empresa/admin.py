37ac22715c7cc6a4db05a8701836a67cd9a6a2a7
Ale26pc <alexandrapc1326@gmaill.com>
2026-03-10 18:29:16 -0500

    fix: archivo admin.py empresa

--- apps/empresa/admin.py
+++ apps/empresa/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Empresa
 
-# Register your models here.
+
+@admin.register(Empresa)
+class EmpresaAdmin(admin.ModelAdmin):
+    list_display = [
+        'nombre',
+        'estado',
+        'estado_empresa',
+        'usuario_registra',
+        'fecha_registro',
+        'usuario_edita',
+        'fecha_edita',
+        'usuario_elimina',
+        'fecha_elimina',
+    ]
+    list_filter = ['estado', 'estado_empresa']
+    search_fields = ['nombre']
+    readonly_fields = [
+        'usuario_registra',
+        'usuario_edita',
+        'usuario_elimina',
+        'fecha_registro',
+        'fecha_edita',
+        'fecha_elimina',
+    ]
+    ordering = ['nombre']
