77a9ba776cb2f1c6a3b2dfe814767432ae7b232c
Jose Ochoa <jochoacairoza492@gmail.com>
2026-03-11 10:20:30 -0500

    feat/archivo admin.py

--- apps/servicio/admin.py
+++ apps/servicio/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Servicio
 
-# Register your models here.
+
+@admin.register(Servicio)
+class ServicioAdmin(admin.ModelAdmin):
+    list_display = [
+        'nombre',
+        'empresa',
+        'estado',
+        'estado_servicio',
+        'usuario_registra',
+        'fecha_registro',
+    ]
+    list_filter = ['estado', 'estado_servicio', 'empresa']
+    search_fields = ['nombre', 'empresa__nombre']
+    list_select_related = ['empresa', 'usuario_registra', 'usuario_edita', 'usuario_elimina']
+    readonly_fields = [
+        'fecha_registro',
+        'fecha_edita',
+        'fecha_elimina',
+        'usuario_registra',
+        'usuario_edita',
+        'usuario_elimina',
+    ]
+    ordering = ['nombre']
+
+    fieldsets = (
+        (None, {
+            'fields': ('nombre', 'estado', 'estado_servicio', 'empresa'),
+        }),
+        ('Auditoría', {
+            'fields': (
+                'usuario_registra',
+                'usuario_edita',
+                'usuario_elimina',
+                'fecha_registro',
+                'fecha_edita',
+                'fecha_elimina',
+            ),
+            'classes': ('collapse',),
+        }),
+    )
